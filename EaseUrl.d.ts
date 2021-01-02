49981b27e9542c1c1cfe6348ec23f83907964fca
yongsheng.li <liyongsheng@me.com>
2021-01-02 17:46:37 +0800

    feat: 增加typescript支持

---
+++ EaseUrl.d.ts
@@ -0,0 +1,53 @@
+export type Url = {
+    path: string,
+    host: string,
+    port: number,
+    hash: string,
+    scheme?: string,
+    query?: Array<{key: string, value: string}>
+}
+
+export enum HashPos {
+    BEFORE,
+    AFTER
+}
+
+export class EaseUrl {
+
+    static parse(url?: string | Url)
+
+    public path: string
+    public host: string
+    public hash: string
+    public scheme: string
+    public query: Query
+
+    public set port(v: number)
+    public get port()
+    public get origin() : string
+    public get url() : string
+
+    constructor(url?: string | Url )
+    parse(url: string | Url) : void
+
+}
+
+export type QueryParam = {
+    key: string, 
+    value: string | number
+}
+
+export class Query {
+
+    public params: Array<QueryParam>
+
+    public get dict() : {[key: string]: string | number}
+    public get search() : string
+
+    constructor(query: string | Array<QueryParam> | Object)
+    public parse(query: string | Array<QueryParam> | Object)
+
+    public add(key: string, value: string | number)
+    public remove(key: string)
+
+}
